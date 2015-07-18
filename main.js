7eba1a5a4c767d7b47fafe7fd1b26b7eb5c34ab9
Ben Ogle <ogle.ben@gmail.com>
2015-07-18 16:46:16 -0700

    Handle an —environment cli option

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
-require('electron-compile').init();
-var Application = require('./src/browser/application');
+var path = require('path')
+
+var getEnvironment = function() {
+  var environment = 'production'
+  for (var i = 0; i < process.argv.length; i++) {
+    if (process.argv[i] == '--environment' && process.argv[i + 1]) {
+      environment = process.argv[i + 1]
+      break
+    }
+  }
+  return environment
+}
+
+if (getEnvironment() == 'production') {
+  require('electron-compile').initForProduction(path.join(__dirname, 'compile-cache'))
+}
+else {
+  console.log('In development mode')
+  require('electron-compile').init()
+}
+
+var Application = require('./src/browser/application')
 application = new Application
