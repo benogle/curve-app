d90d655f7a761cb1f98a77886700aaefd24feabd
Ben Ogle <ogle.ben@gmail.com>
2015-08-01 15:31:56 -0700

    Log out the fileName for now

--- src/main-window/main.js
+++ src/main-window/main.js
@@ -1,15 +1,22 @@
+var SVGDocument, hash, args;
+
 SVGDocument = require('curve').SVGDocument
 
-var canvas, doc, Examples;
+hash = window.location.hash.slice(1)
+args = Object.freeze(JSON.parse(decodeURIComponent(hash)))
 
-canvas = document.createElement('div');
-canvas.id = 'canvas';
-document.body.appendChild(canvas);
+console.log(args.fileName);
 
-window.DOC = doc = new SVGDocument("canvas");
-
-Examples = {
-  rects: '<svg height="1024" width="1024" xmlns="http://www.w3.org/2000/svg"><rect x="20" y="30" width="200" height="400" fill="red" /></svg>',
-};
-
-doc.deserialize(Examples.rects);
+// var canvas, doc, Examples;
+//
+// canvas = document.createElement('div');
+// canvas.id = 'canvas';
+// document.body.appendChild(canvas);
+//
+// window.DOC = doc = new SVGDocument("canvas");
+//
+// Examples = {
+//   rects: '<svg height="1024" width="1024" xmlns="http://www.w3.org/2000/svg"><rect x="20" y="30" width="200" height="400" fill="red" /></svg>',
+// };
+//
+// doc.deserialize(Examples.rects);
