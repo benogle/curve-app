a560c293d78320356bbc3afdfb16a00544a336e6
Ben Ogle <ogle.ben@gmail.com>
2015-08-02 16:12:05 -0700

    Pull a model out of the SVGEditor

--- src/editor-window/index.js
+++ src/editor-window/index.js
@@ -1,7 +1,11 @@
+var Curve = require('./curve');
 var SVGEditor = require('./svg-editor');
 
 window.onload = function() {
-  var hash = window.location.hash.slice(1)
-  var args = Object.freeze(JSON.parse(decodeURIComponent(hash)))
-  new SVGEditor(args.fileName)
+  var hash, args, editor
+  hash = window.location.hash.slice(1)
+  args = Object.freeze(JSON.parse(decodeURIComponent(hash)))
+  global.curve = new Curve(args)
+  global.EDITOR = editor = new SVGEditor(args.fileName)
+  document.body.appendChild(editor.getCanvas())
 }
--- src/editor-window/svg-editor.js
+++ src/editor-window/svg-editor.js
@@ -1,48 +1,69 @@
 var fs = require('fs');
 var SVGDocument = require('curve').SVGDocument
+var Emitter = require('event-kit').Emitter
+
+class SVGEditorModel {
+  constructor(filePath) {
+    this.filePath = filePath
+    this.emitter = new Emitter
+  }
+
+  onDidChangeFilePath(callback) {
+    this.emitter.on('did-change-file-path', callback)
+  }
+
+  getFilePath() {
+    return this.filePath
+  }
+
+  setFilePath(filePath) {
+    if(this.filePath === filePath) return;
+
+    this.filePath = filePath
+    this.emitter.emit('did-change-file-path', filePath)
+  }
+}
 
 class SVGEditor {
-  constructor(svgPath) {
-    this.svgPath = svgPath;
-    window.EDITOR = this
+  constructor(filePath) {
+    this.model = new SVGEditorModel(filePath)
+    this.model.onDidChangeFilePath(this.updateTitle.bind(this))
 
     this.updateTitle()
-    this.createDocument()
+    this.createCanvas()
     this.deserialize()
   }
 
-  deserialize() {
-    if (!this.svgPath) return;
-    try {
-      var svg = fs.readFileSync(this.svgPath, {encoding: 'utf8'});
-      this.svgDocument.deserialize(svg);
-    }
-    catch (error) {
-      console.error(error.message);
-    }
+  getCanvas() {
+    return this.canvas
   }
 
-  createDocument() {
+  createCanvas() {
     this.canvas = document.createElement('div');
     this.canvas.id = 'canvas'
-    document.body.appendChild(this.canvas);
     this.svgDocument = new SVGDocument(this.canvas);
   }
 
+  deserialize() {
+    let filePath = this.model.getFilePath()
+    if (!filePath) return;
+
+    try {
+      var svg = fs.readFileSync(filePath, {encoding: 'utf8'});
+      this.svgDocument.deserialize(svg);
+    }
+    catch (error) {
+      console.error(error.stack);
+    }
+  }
+
+  getTitle() {
+    return `${this.model.getFilePath() || 'untitled'} - Curve`
+  }
+
   updateTitle() {
-    document.title = `${this.svgPath || 'untitled'} - Curve`
+    document.title = this.getTitle()
   }
 }
 
 module.exports = SVGEditor;
-
-// var canvas, doc, Examples;
-//
-//
-// window.DOC = doc = new SVGDocument("canvas");
-//
-// Examples = {
-//   rects: '<svg height="1024" width="1024" xmlns="http://www.w3.org/2000/svg"><rect x="20" y="30" width="200" height="400" fill="red" /></svg>',
-// };
-//
-// doc.deserialize(Examples.rects);
