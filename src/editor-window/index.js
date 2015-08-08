1680b48f2460c880792483de8275e0f2b9e2c844
Ben Ogle <ogle.ben@gmail.com>
2015-08-08 15:29:04 -0700

    Add platform class to the body

--- src/editor-window/index.js
+++ src/editor-window/index.js
@@ -5,6 +5,9 @@ window.onload = function() {
   var hash, args, editor
   hash = window.location.hash.slice(1)
   args = Object.freeze(JSON.parse(decodeURIComponent(hash)))
+
+  document.body.classList.add(`platform-${process.platform}`)
+
   global.curve = new Curve(args)
   editor = new SVGEditor(args.fileName, document.querySelector('#canvas'))
   global.EDITOR = editor // debugging
